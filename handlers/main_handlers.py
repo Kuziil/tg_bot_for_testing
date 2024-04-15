0df36a237df74299b423b6c12356f35078757aff
kuziil <sikuz70@gmail.com>
2024-04-15 08:38:56 +0300

    reaction to pressing the start button

--- handlers/main_handlers.py
+++ handlers/main_handlers.py
@@ -1,5 +1,5 @@
-from aiogram import Router
-from aiogram.types import Message
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import StateFilter, Command
 from aiogram.fsm.state import default_state
 from fluentogram import TranslatorRunner
@@ -15,4 +15,9 @@ async def process_start_command(message: Message, i18n: TranslatorRunner):
     await message.answer(
         text=i18n.command.start(),
         reply_markup=create_start_kb(i18n=i18n)
-        )
+    )
+
+
+@router.callback_query(F.data == "button-start")
+async def process_press_start_button(callback: CallbackQuery):
+    await callback.message.answer(text="11111")
